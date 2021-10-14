c16c6c9bd87483bde83b4a8eac1220210000e79a
AdrianAparisi <aparisiadrian@gmail.com>
2021-10-14 15:38:13 +0200

    Calculadora 3 -Funcionando

--- calculadora.js
+++ calculadora.js
@@ -1,135 +1,131 @@
 'use strict'
 
 alert("Hola usuario, bienvenido a la calculadora.")
-let repeat = true;
+let repeat = false;
 let operacion;
 let operadores;
 let operador1 = '';
 let operador2 = '';
-let salir = false;
-let lastResult = 0;
+let lastResult;
+let Calculator = new Object();
 
-
-function pedirOperacion() {
-    while (operacion !== '+' && operacion !== '-' && operacion !== '*' && operacion !== '/') {
-        operacion = prompt("¿Que operación quieres realizar? ( +, -, * o /.)");
-        operacion = operacion.trim();
-
-        if (operacion === '+' || operacion === '-' || operacion === '*' || operacion === '/') {
-
-        } else {
-            alert("No es un operando válido...")
-        }
-    }
-}
-
-function validar() {
-    operador1 = "";
-    operador2 = "";
-    let operadores = prompt('Introduce los numeros separados por espacio', '');
-
-
-    for (let i = 0; i < operadores.length; i++) {
-        if (operadores[i] !== ' ') {
-            operador1 += operadores[i];
-        } else {
-            if (operador1 !== '') {
-                break;
-            }
-        }
-    }
-
-    console.log(operador1);
-
-    for (; i < operadores.length; i++) {
-        if (operadores[i] !== ' ') {
-            operador2 += operadores[i];
-        } else {
-            if (operador2 !== '') {
-                break;
-            }
-        }
-    }
-
-    console.log(operador2);
-
-    operador1 = Number(operador1);
-    operador2 = Number(operador2);
-
-    if (isNaN(operador1) || isNaN(operador2)) {
-        alert('Tienes que introducir números.')
-        return false;
-    } else {
-        return true;
-    }
-}
-
-let calculator = {
-    sum(operadores) {
-        let operador1 = Number(operadores[0]);
-        let operador2 = Number(operadores[1]);
-        resultado = operador1 + operador2;
-        alert('El resultado es: ' + resultado());
-        return resultado;
+Calculator = {
+    sum() {
+        lastResult = operador1 + operador2;
+        alert('El resultado de la suma es: ' + lastResult);
     },
 
-    res(operadores) {
-        let operador1 = Number(operadores[0]);
-        let operador2 = Number(operadores[1]);
-        resultado = operador1 - operador2;
-        alert('El resultado es: ' + resultado());
-        return resultado;
+    res() {
+        lastResult = operador1 - operador2;
+        alert('El resultado de la resta es: ' + lastResult);
     },
 
-    mul(operadores) {
-        let operador1 = Number(operadores[0]);
-        let operador2 = Number(operadores[1]);
-        resultado = operador1 * operador2;
-        alert('El resultado es: ' + resultado());
-        return resultado;
+
+    mul() {
+        lastResult = operador1 * operador2;
+        alert('El resultado de la multiplicacion es: ' + lastResult);
     },
 
-    div(operadores) {
-        let operador1 = Number(operadores[0]);
-        let operador2 = Number(operadores[1]);
-        resultado = operador1 / operador2;
-        alert('El resultado es: ' + resultado());
-        return resultado;
+    div() {
+        lastResult = operador1 / operador2;
+        alert('El resultado es de la división es: ' + lastResult);
     },
-    lastResult
+
 };
 
-while (repeat) {
+do {
     pedirOperacion();
-    let pedirOperadores = validar();
-
-    while (!pedirOperadores) {
-        pedirOperadores = validar();
-    }
-    let resultado;
+    validar();
 
     switch (operacion) {
         case '+':
-            calculator.lastResult = calculator.sum(operadores);
+            Calculator.sum();
             break;
 
         case '-':
-            calculator.lastResult = calculator.res(operadores);
+            Calculator.res();
             break;
 
         case '*':
-            calculator.lastResult = calculator.mul(operadores);
+            Calculator.mul();
             break;
 
         case '/':
-            calculator.lastResult = calculator.div(operadores);
+            Calculator.div();
             break;
 
     }
-
     repeat = confirm('Quieres realizar otra operación?');
+} while ((operacion !== '+' && operacion !== '-' && operacion !== '*' && operacion !== '/') || repeat);
 
+
+function pedirOperacion() {
+    let operandoValido = false;
+
+    while (!operandoValido) {
+        operacion = prompt("¿Que operación quieres realizar? ( +, -, * o /.)");
+        operacion = operacion.trim();
+
+        if (operacion !== '+' && operacion !== '-' && operacion !== '*' && operacion !== '/') {
+            alert("No es un operando válido...")
+            operandoValido = false;
+        } else {
+            operandoValido = true;
+        }
+    }
 }
-operadores = validar();
 
-console.log(operacion);
+
+function validar() {
+    operador1 = '';
+    operador2 = '';
+    let exit = false;
+
+    while (!exit) {
+        let operadores = prompt('Introduce los numeros separados por espacio', '');
+
+        let i = 0;
+        for (; i < operadores.length; i++) {
+            if (operadores[i] === 'r' || operadores[i] === 'R') {
+                operador1 = lastResult;
+                break;
+            }
+            if (operadores[i] !== ' ') {
+                operador1 += operadores[i];
+            } else {
+                if (operador1 !== '') {
+                    break;
+                }
+            }
+        } i++;
+
+        console.log(operador1);
+
+        for (; i < operadores.length; i++) {
+            if (operadores[i] === 'r' || operadores[i] === 'R') {
+                operador2 = lastResult;
+                break;
+            }
+            if (operadores[i] !== ' ') {
+                operador2 += operadores[i];
+            } else {
+                if (operador2 !== '') {
+                    break;
+                }
+            }
+        }
+
+        console.log(operador2);
+
+        operador1 = Number(operador1);
+        operador2 = Number(operador2);
+
+        if (isNaN(operador1) || isNaN(operador2)) {
+            alert('Tienes que introducir números.')
+            exit = false;
+        } else {
+            exit = true;
+        }
+    }
+    console.log(operacion);
+}
